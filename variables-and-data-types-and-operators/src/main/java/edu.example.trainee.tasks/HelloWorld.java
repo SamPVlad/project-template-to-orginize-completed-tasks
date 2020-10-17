20529d1d3292ba932408a90df41aae89783ce2f3
ValentynNagay <nagayvalentyn@gmail.com>
2020-10-17 19:22:28 +0300

    [Common] Create a basic project structure

---
+++ variables-and-data-types-and-operators/src/main/java/edu.example.trainee.tasks/HelloWorld.java
@@ -0,0 +1,7 @@
+package edu.example.trainee.tasks;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi from the 'variables-and-data-types-and-operators' module");
+    }
+}
